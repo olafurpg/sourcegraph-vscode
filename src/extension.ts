c366ce11a29b47f482ec380863c92d5d5c9162b5
Ólafur Páll Geirsson <olafurpg@gmail.com>
2021-09-09 10:20:58 +0200

    feat: wip

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import open from 'open'
 import * as vscode from 'vscode'
 import { getSourcegraphUrl } from './config'
 import { repoInfo } from './git'
+import fetch from 'node-fetch'
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
 const { version } = require('../package.json')
@@ -19,7 +20,9 @@ const handleCommandErrors = <P extends unknown[], R>(command: (...args: P) => Pr
     try {
         return await command(...args)
     } catch (error) {
-        await showError(error)
+        if (error instanceof Error) {
+            await showError(error)
+        }
     }
 }
 
@@ -50,6 +53,107 @@ async function openCommand(): Promise<void> {
             `&end_row=${encodeURIComponent(String(editor.selection.end.line))}` +
             `&end_col=${encodeURIComponent(String(editor.selection.end.character))}`
     )
+}
+
+async function browseCommand(): Promise<void> {
+    const clipboard = await vscode.env.clipboard.readText()
+    const value = clipboard.startsWith('https://sourcegraph.com') ? clipboard : ''
+    const input = await vscode.window.showInputBox({
+        value,
+    })
+
+    if (input) {
+        const uri = vscode.Uri.parse(input.replace('https://', 'sourcegraph://'))
+        const textDocument = await vscode.workspace.openTextDocument(uri)
+        await vscode.window.showTextDocument(textDocument)
+    }
+}
+
+async function graphqlQuery<A, B>(query: string, variables: A): Promise<B | undefined> {
+    const accessToken = process.env.SRC_ACCESS_TOKEN
+    if (!accessToken) {
+        await vscode.window.showErrorMessage('no SRC_ACCESS_TOKEN')
+        return
+    }
+    const endpoint = process.env.SRC_ENDPOINT
+    if (!endpoint) {
+        await vscode.window.showErrorMessage('no SRC_ENDPOINT')
+        return
+    }
+    const result = await fetch(endpoint, {
+        method: 'post',
+        body: JSON.stringify({
+            query: {},
+            variables: {
+                repository: '',
+                commit: '',
+                path: '',
+            },
+        }),
+        headers: {
+            Authorization: `token ${accessToken}`,
+        },
+    })
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const response: any = await result.json()
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return response
+}
+
+interface ParsedRepoURI {
+    repoName: string
+    revision: string | undefined
+    commitID: string | undefined
+    filePath: string | undefined
+    position: vscode.Position | undefined
+    range: vscode.Range | undefined
+}
+
+export function parseRepoURI(parsed: URL): ParsedRepoURI {
+    const repoName = parsed.hostname + decodeURIComponent(parsed.pathname)
+    const revision = decodeURIComponent(parsed.search.slice('?'.length)) || undefined
+    let commitID: string | undefined
+    if (revision?.match(/[\dA-f]{40}/)) {
+        commitID = revision
+    }
+    const fragmentSplit = parsed.hash.slice('#'.length).split(':').map(decodeURIComponent)
+    let filePath: string | undefined
+    let position: vscode.Position | undefined
+    let range: vscode.Range | undefined
+    if (fragmentSplit.length === 1) {
+        filePath = fragmentSplit[0]
+    }
+    if (fragmentSplit.length === 2) {
+        filePath = fragmentSplit[0]
+        const rangeOrPosition = fragmentSplit[1]
+        const rangeOrPositionSplit = rangeOrPosition.split('-')
+
+        if (rangeOrPositionSplit.length === 1) {
+            position = parsePosition(rangeOrPositionSplit[0])
+        }
+        if (rangeOrPositionSplit.length === 2) {
+            range = new vscode.Range(parsePosition(rangeOrPositionSplit[0]), parsePosition(rangeOrPositionSplit[1]))
+        }
+        if (rangeOrPositionSplit.length > 2) {
+            throw new Error('unexpected range or position: ' + rangeOrPosition)
+        }
+    }
+    if (fragmentSplit.length > 2) {
+        throw new Error('unexpected fragment: ' + parsed.hash)
+    }
+
+    return { repoName, revision, commitID, filePath: filePath || undefined, position, range }
+}
+
+const parsePosition = (string: string): vscode.Position => {
+    const split = string.split(',')
+    if (split.length === 1) {
+        return new vscode.Position(parseInt(string, 10), 0)
+    }
+    if (split.length === 2) {
+        return new vscode.Position(parseInt(split[0], 10), parseInt(split[1], 10))
+    }
+    throw new Error('unexpected position: ' + string)
 }
 
 /**
@@ -90,6 +194,94 @@ export function activate(context: vscode.ExtensionContext): void {
     // Register our extension commands (see package.json).
     context.subscriptions.push(vscode.commands.registerCommand('extension.open', handleCommandErrors(openCommand)))
     context.subscriptions.push(vscode.commands.registerCommand('extension.search', handleCommandErrors(searchCommand)))
+    context.subscriptions.push(vscode.commands.registerCommand('extension.browse', handleCommandErrors(browseCommand)))
+    vscode.workspace.registerFileSystemProvider('sourcegraph', new SourcegraphFileSystemProvider(), {
+        isReadonly: true,
+    })
+}
+
+// interface StatParameters {}
+// interface StatResult {}
+// interface RevisionParameters {}
+// interface RevisionResult {
+//     revision: string
+// }
+interface BlobParameters {
+    repository: string
+    revision: string
+    path: string
+}
+interface BlobResult {
+    data: {
+        repository: {
+            commit: {
+                blob: {
+                    content: string
+                }
+            }
+        }
+    }
+}
+const ContentQuery = `query Content($repository: String!, $revision: String!, $path: String!) {
+    repository(name: $repository) {
+      commit(rev: $revision) {
+        blob(path: $path) {
+          content
+        }
+      }
+    }
+  }
+  `
+
+class SourcegraphFileSystemProvider implements vscode.FileSystemProvider {
+    private _emitter = new vscode.EventEmitter<vscode.FileChangeEvent[]>()
+    public readonly onDidChangeFile: vscode.Event<vscode.FileChangeEvent[]> = this._emitter.event
+    public stat(uri: vscode.Uri): Promise<vscode.FileStat> {
+        const time = new Date().getMilliseconds()
+        return Promise.resolve({
+            mtime: time,
+            ctime: time,
+            size: 123,
+            type: vscode.FileType.File,
+        })
+    }
+    public async readFile(uri: vscode.Uri): Promise<Uint8Array> {
+        // const parsed = parseRepoURI(new URL(uri.toString()))
+        // const revision: string = parsed.revision || (await graphqlQuery<RevisionParameters, RevisionResult>('', {}))?.revision || ''
+        const result = await graphqlQuery<BlobParameters, BlobResult>(ContentQuery, {
+            repository: 'github.com/Netflix/Hystrix',
+            revision: 'master',
+            path: 'hystrix-core/src/main/java/com/netflix/hystrix/AbstractCommand.java',
+        })
+        if (result) {
+            const encoder = new TextEncoder()
+            return encoder.encode(result.data.repository.commit.blob.content)
+        }
+        throw new Error('Method not implemented.')
+    }
+    public readDirectory(uri: vscode.Uri): [string, vscode.FileType][] | Thenable<[string, vscode.FileType][]> {
+        return []
+    }
+
+    public createDirectory(uri: vscode.Uri): void | Thenable<void> {
+        throw new Error('Method not implemented.')
+    }
+    public writeFile(
+        uri: vscode.Uri,
+        content: Uint8Array,
+        options: { create: boolean; overwrite: boolean }
+    ): void | Thenable<void> {
+        throw new Error('Method not implemented.')
+    }
+    public delete(uri: vscode.Uri, options: { recursive: boolean }): void | Thenable<void> {
+        throw new Error('Method not implemented.')
+    }
+    public rename(oldUri: vscode.Uri, newUri: vscode.Uri, options: { overwrite: boolean }): void | Thenable<void> {
+        throw new Error('Method not implemented.')
+    }
+    public watch(uri: vscode.Uri, options: { recursive: boolean; excludes: string[] }): vscode.Disposable {
+        throw new Error('Method not implemented.')
+    }
 }
 
 export function deactivate(): void {
