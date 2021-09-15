0c620ed3c68258990c57eb4d003c4b5d5026de5a
Ólafur Páll Geirsson <olafurpg@gmail.com>
2021-09-15 12:13:36 +0200

    feat: wip

--- src/browse/SourcegraphCompletionItemProvider.ts
+++ src/browse/SourcegraphCompletionItemProvider.ts
@@ -1,20 +1,17 @@
 import * as vscode from 'vscode'
-import { log } from '../log'
 import { getCompletionItems } from './highlighting/completion'
 import { scanSearchQuery, SearchPatternType } from './highlighting/scanner'
+
 export class SourcegraphCompletionItemProvider implements vscode.CompletionItemProvider {
     public async provideCompletionItems(
         document: vscode.TextDocument,
         position: vscode.Position,
-        token: vscode.CancellationToken,
-        context: vscode.CompletionContext
+        _token: vscode.CancellationToken,
+        _context: vscode.CompletionContext
     ): Promise<vscode.CompletionList | null> {
         const scanned = scanSearchQuery(document.getText(), true, SearchPatternType.literal)
         if (scanned.type === 'success') {
             const items = await getCompletionItems(scanned.term, position, true, true)
-            for (const item of items?.items || []) {
-                log.appendLine(`COMPLETION ITEM ${JSON.stringify(item)}`)
-            }
             return items
         }
         return null
--- src/browse/graphqlQuery.ts
+++ src/browse/graphqlQuery.ts
@@ -124,7 +124,7 @@ export async function searchHtml(
     const result = await graphqlQuery<SearchParameters, SearchResult>(searchQuery(patternType), { query }, token)
     const html: string[] = []
     const nodes = result?.data?.search?.results?.results
-    for (const node of (nodes || []).slice(0, 4)) {
+    for (const node of nodes || []) {
         const url = node?.file?.url
         if (!url) {
             continue
@@ -133,10 +133,8 @@ export async function searchHtml(
         if (lineMatches.length === 0) {
             continue
         }
-
-        html.push('<p>')
-        html.push(`<code>${url}</code>`)
-        html.push('<pre>')
+        let first = true
+        let filenameMatchesCount = 0
         for (const [lineMatchIndex, lineMatch] of lineMatches.entries()) {
             const line = lineMatch.lineNumber
             if (!line) {
@@ -146,12 +144,12 @@ export async function searchHtml(
             if (!preview) {
                 continue
             }
-            if (lineMatchIndex > 0) {
-                html.push('\n')
-            }
             const uri = `sourcegraph://${host}${url}?L${line + 1}:0`
             let index = 0
             const highlightedPreview: string[] = []
+            if (lineMatchIndex > 0) {
+                highlightedPreview.push('\n')
+            }
             highlightedPreview.push(`L${line}: `)
             for (const offsetsAndLength of lineMatch.offsetAndLengths || []) {
                 const [start, length] = offsetsAndLength
@@ -163,9 +161,19 @@ export async function searchHtml(
                 index = end
             }
             highlightedPreview.push(escapeHtml(preview.slice(index, preview.length)))
+            if (first) {
+                first = false
+                html.push('<p>')
+                html.push(`<code>${url}</code>`)
+                html.push('<pre>')
+            }
             html.push(
                 `<a id='${uri}' style='cursor:pointer' class='sourcegraph-location'>${highlightedPreview.join('')}</a>`
             )
+            filenameMatchesCount++
+            if (filenameMatchesCount > 5) {
+                break
+            }
         }
         html.push('</pre>')
         html.push('</p>')
